e025e530606b9636e8053a407f0db73df078b36d
Carole <cjpillette@hotmail.com>
2017-06-07 19:11:54 +1000

    Created Fish and order components

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,62 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes.js';
+
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+    //initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //update our state - takes a copy first
+    const fishes = {...this.state.fishes};
+    //add a new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    //set state
+    this.setState({fishes});
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
+  addToOrder(key){
+    //take a copy of our state
+    const order = {...this.state.order};
+    //update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    //update our setState
+    this.setState({order});
+  }
   render() {
     return(
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="fresh seafood market"/>
+          <ul className="list-of-fishes">
+          {
+            Object
+            .keys(this.state.fishes)
+            .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+          }
+          </ul>
         </div>
-        <Order />
-        <Inventory />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
 
     )
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('you changed the url');
+    //first grab the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    //second we're going to transition from / to /store/:id
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
       <h2>Please enter a Store</h2>
-      <input type="text" required placeholder="Store Name" />
+      <input type="text" required placeholder="Store Name" defaultValue={
+        getFunName()} ref={(input) => {this.storeInput = input} }/>
       <button type="Submit">Visit Store</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
