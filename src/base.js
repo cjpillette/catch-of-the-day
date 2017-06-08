d127d9aa49a4c52495d09c17b805004769049996
Carole <cjpillette@hotmail.com>
2017-06-08 13:13:59 +1000

    set up firebase within the app

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyA015Q0z1I7KRsBdoUqfswNJXmr__FI0N4",
+  authDomain: "catch-of-the-day-carole-p.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-carole-p.firebaseio.com"
+});
+
+export default base;
